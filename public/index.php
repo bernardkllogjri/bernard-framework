b0920726ff9405c6c6c07dcd0cf3afe98ea16ed1
Bernard <bernard.kllogjri@sotlab.al>
2018-10-28 14:39:26 +0100

    kot

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
 
-  require '../src/bootstrap.php';
+  require '../vendor/autoload.php';
+  require '../vendor/bernardkllogjri/framesource/src/bootstrap.php';
   return Router\Router::handleTraffic();
